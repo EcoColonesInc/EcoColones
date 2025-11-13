6acff39e407dfd2e7dac042652d660df3f1bf7df
David S <davidsr4508@hotmail.com>
2025-11-13 13:15:28 -0600

    Refactor affiliate dashboard: update imports,, and implement redeem points functionality.

--- app/(affliates)/affiliate/dashboard/page.tsx
+++ app/(affliates)/affiliate/dashboard/page.tsx
@@ -1,6 +1,18 @@
-import { CustomTable } from "@/components/custom/customTable";
-export default function Page() {
-    
+import { CustomTable } from "@/components/custom/affiliate/affiliateTable";
+import { PointsExchangeCards } from "@/components/custom/affiliate/redeemPoints";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+
+    const supabase = await createClient();
+    const{ data:{ user } ,error} = await supabase.auth.getUser();
+
+    if (error || !user) {
+        redirect('/login');
+    }
+
+
     const columns = [
         { header: 'Numero de Transacción', accessorKey: 'transactionID' },
         { header: 'Usuario', accessorKey: 'user' },
@@ -11,7 +23,13 @@ export default function Page() {
         { header: 'Estado', accessorKey: 'status' },
     ];
 
-    const mockdata = [
+
+
+    //TODO: Hay que cambiar estos valores por llamadas a la API
+    const monthlyTotal = 1500; // Ejemplo de total mensual de EcoColones
+    const exchangeRate = 0.05; // Ejemplo de tasa de cambio EcoColones a moneda local
+
+    const mockdata = [ //TODO:Hay que cambiar esto por llamadas a la API
         {
             transactionID: 'TXN123456',
             user: 'Maria Lopez',
@@ -28,7 +46,7 @@ export default function Page() {
             quantity: 10,
             date: '2024-06-15',
             amount: '2500',
-            status: 'Completado',
+            status: 'Pendiente',
         },
         {
             transactionID: 'TXN123456',
@@ -37,82 +55,32 @@ export default function Page() {
             quantity: 10,
             date: '2024-06-15',
             amount: '2500',
-            status: 'Completado',
-        },
-        {
-            transactionID: 'TXN123456',
-            user: 'Maria Lopez',
-            product: 'Crunchy Tacos',
-            quantity: 10,
-            date: '2024-06-15',
-            amount: '2500',
-            status: 'Completado',
-        },
-        {
-            transactionID: 'TXN123456',
-            user: 'Maria Lopez',
-            product: 'Crunchy Tacos',
-            quantity: 10,
-            date: '2024-06-15',
-            amount: '2500',
-            status: 'Completado',
-        },
-        {
-            transactionID: 'TXN123456',
-            user: 'Maria Lopez',
-            product: 'Crunchy Tacos',
-            quantity: 10,
-            date: '2024-06-15',
-            amount: '2500',
-            status: 'Completado',
-        },
-        {
-            transactionID: 'TXN123456',
-            user: 'Maria Lopez',
-            product: 'Crunchy Tacos',
-            quantity: 10,
-            date: '2024-06-15',
-            amount: '2500',
-            status: 'Completado',
-        },
-        {
-            transactionID: 'TXN123456',
-            user: 'Maria Lopez',
-            product: 'Crunchy Tacos',
-            quantity: 10,
-            date: '2024-06-15',
-            amount: '2500',
-            status: 'Completado',
-        },
-        {
-            transactionID: 'TXN123456',
-            user: 'Maria Lopez',
-            product: 'Crunchy Tacos',
-            quantity: 10,
-            date: '2024-06-15',
-            amount: '2500',
-            status: 'Completado',
-        },
-        {
-            transactionID: 'TXN123456',
-            user: 'Maria Lopez',
-            product: 'Crunchy Tacos',
-            quantity: 10,
-            date: '2024-06-15',
-            amount: '2500',
-            status: 'Completado',
-        },
+            status: 'Cancelado',
+        }
     ];
 
 
     return (
     <div className="container mx-auto px-4 space-y-12 md:space-y-20">
-      <h1 className="text-2xl font-bold mb-4">Panel de Control</h1>
-      <h2 className="text-lg text-gray-600 mb-10">¡Bienvenido! Gestiona las opciones de tu comercio desde aquí</h2>
-      <div className="bg-green-50 border border-gray-300 rounded-lg p-6 shadow-md">
-        <h3 className="text-xl font-semibold mb-4">Transacciones recientes</h3>
-        <CustomTable columns={columns} data={mockdata} />
-      </div>
+        <h1 className="text-2xl font-bold mb-4 pt-10">Panel de Control</h1>
+        <h2 className="text-lg text-gray-600 mb-5">¡Bienvenido! Gestiona las opciones de tu comercio desde aquí</h2>
+        {/* This is the table container*/}
+        <div className="bg-green-50 min-h-96 max-h-96 border border-gray-300 rounded-lg p-6 shadow-md"> 
+            <h3 className="text-xl font-semibold mb-4">Transacciones recientes</h3>
+            <CustomTable columns={columns} data={mockdata} />
+        </div>
+
+        {/* This is the redeem points section */}
+        
+        <div>
+            <h3 className="text-xl font-semibold mb-4">Canje de Puntos</h3>
+            <PointsExchangeCards monthlyTotal={monthlyTotal} exchangeRate={exchangeRate} />
+        </div>
+        
+        {/* This is the products section */}
+        <div>
+            Sección de productos
+        </div>
     </div>
   );
 
